e54e0d027826f856bf5c0e36b751da1f6a0a91b8
delsananthony <delsan.anthony.das@gmail.com>
2025-03-15 08:40:22 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Filename: main.py
+Author: Delsan Anthony Soliva
+Created: 2025-03-15
+Version: v0.1.0
+Description: A hello world API
+'''
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def home():
+    return {'message': 'Hello World'}
